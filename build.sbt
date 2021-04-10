553e41ae150f587d9ac24aa3978dba75a79fc26a
Christopher Davenport <chris@christopherdavenport.tech>
2021-04-10 15:42:45 -0700

    Wherever I am

--- build.sbt
+++ build.sbt
@@ -1,25 +1,27 @@
 inThisBuild(
   Seq(
     organization := "org.http4s",
-    crossScalaVersions := Seq("2.13.2"),
+    crossScalaVersions := Seq("2.13.4"),
     scalaVersion := crossScalaVersions.value.head,
     testFrameworks += new TestFramework("munit.Framework"),
-    addCompilerPlugin(("org.typelevel" % "kind-projector" % "0.11.0").cross(CrossVersion.full)),
+    addCompilerPlugin(("org.typelevel" % "kind-projector" % "0.11.3").cross(CrossVersion.full)),
     addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
- )
+  )
 )
 
-val http4sVersion = "0.21.6"
-val netty = "0.1.0"
-val finagle = "0.21.4-20.5.0-0"
+val http4sVersion = "0.21-127-a5ca9fc-SNAPSHOT"
+// val netty = "0.1.0"
+// val finagle = "0.21.4-20.5.0-0"
 
 libraryDependencies ++= List(
   "org.http4s" %% "http4s-ember-server" % http4sVersion,
+  "org.http4s" %% "http4s-ember-client" % http4sVersion,
   "org.http4s" %% "http4s-blaze-server" % http4sVersion,
-  "org.http4s" %% "http4s-jetty" % http4sVersion,
-  "org.http4s" %% "http4s-tomcat" % http4sVersion,
-  "org.http4s" %% "http4s-netty-server" % netty,
-  "org.http4s" %% "http4s-finagle" % finagle,
+  "org.http4s" %% "http4s-dsl" % http4sVersion,
+  // "org.http4s" %% "http4s-jetty" % http4sVersion,
+  // "org.http4s" %% "http4s-tomcat" % http4sVersion,
+  // "org.http4s" %% "http4s-netty-server" % netty,
+  // "org.http4s" %% "http4s-finagle" % finagle,
   "ch.qos.logback" % "logback-classic" % "1.2.3"
 )
 
--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -2,72 +2,103 @@ package example
 
 import cats.effect.{ExitCode, IO, IOApp}
 import cats.implicits._
-import com.twitter.finagle.{Http => FHttp}
-import com.twitter.util.Await
+// import com.twitter.finagle.{Http => FHttp}
+// import com.twitter.util.Await
 import org.http4s._
 import org.http4s.implicits._
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.server.jetty.JettyBuilder
-import org.http4s.netty.server.NettyServerBuilder
-import org.http4s.server.tomcat.TomcatBuilder
-import org.http4s.finagle._
+// import org.http4s.server.jetty.JettyBuilder
+// import org.http4s.netty.server.NettyServerBuilder
+// import org.http4s.server.tomcat.TomcatBuilder
+// import org.http4s.finagle._
+import scala.concurrent.duration._
 
 object Main {
+  import org.http4s.dsl.io._
+  // private val action = IO.pure(Response[IO](Status.Ok))
+  val sarray = Array.fill(10000)(1.toByte)
+  val barray = Array.fill(10000000)(1.toByte)
   val app = HttpRoutes
     .of[IO] {
-      case req if req.method == Method.GET && req.pathInfo == "/hello" =>
-        IO(Response(Status.Ok).withEntity("Hello World in " + Thread.currentThread().getName))
+      case req@ GET -> Root / "foo" => 
+        Ok()
+        // (req.body.compile.drain, action).mapN{ case (_, resp) => resp}
+      // case req if req.method == Method.GET && req.pathInfo.renderString == "/smallArray" => 
+      //   IO(Response().withEntity(sarray))
+      // case req if req.method == Method.GET && req.pathInfo.renderString == "/bigArray" => 
+      //   IO(Response().withEntity(barray))
+      // case req if req.method == Method.GET && req.pathInfo.renderString == "/hello" =>
+      //   IO(Response(Status.Ok).withEntity("Hello World in " + Thread.currentThread().getName))
     }
     .orNotFound
 }
 
-object NettyTestServer extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] =
-    NettyServerBuilder[IO].withHttpApp(Main.app).resource.use(_ => IO.never)
-}
+// object NettyTestServer extends IOApp {
+//   override def run(args: List[String]): IO[ExitCode] =
+//     NettyServerBuilder[IO].withHttpApp(Main.app).resource.use(_ => IO.never)
+// }
 
-object BlazeTestServer extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] =
-    BlazeServerBuilder[IO](concurrent.ExecutionContext.global)
-      .withHttpApp(Main.app)
-      .resource
-      .use(_ => IO.never)
+// object BlazeTestServer extends IOApp {
+//   override def run(args: List[String]): IO[ExitCode] =
+//     BlazeServerBuilder[IO](concurrent.ExecutionContext.global)
+//       .withHttpApp(Main.app)
+//       .resource
+//       .use(_ => IO.never)
 
-}
+// }
 
 object EmberTestServer extends IOApp {
+  val logger = io.chrisdavenport.log4cats.slf4j.Slf4jLogger.getLogger[IO]
   override def run(args: List[String]): IO[ExitCode] =
     EmberServerBuilder
       .default[IO]
-      .withPort(8080)
-      .withMaxConcurrency(200) //tweak it
+      .withHost("0.0.0.0")
+      .withPort(8081)
+      .withMaxConcurrency(1024) //tweak it
       .withHttpApp(Main.app)
+      .withLogger(logger)
+      .withErrorHandler({
+        case t => logger.error(t)("Unexpected service error").as(Response[IO](Status.InternalServerError))
+      })
+      // .withOnWriteFailure{ case (req, resp, t) => logger.info(t)(s"Failed to write Response: $resp for req: $req")}
       .build
       .use(_ => IO.never)
 }
 
-object JettyTestServer extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] =
-    JettyBuilder[IO]
-      .bindHttp(8080)
-      .mountHttpApp(Main.app, "/")
-      .resource
-      .use(_ => IO.never)
+object EmberClientApp extends IOApp {
+  import org.http4s.implicits._
+  def run(args: List[String]): IO[ExitCode] = 
+    org.http4s.ember.client.EmberClientBuilder.default[IO].build.use{client => 
+      client.run(Request[IO](Method.GET, uri"http://localhost:8081/foo")).use{//.putHeaders(org.http4s.headers.`Content-Length`.zero)).use{
+        resp => 
+        resp.body.compile.drain
+      }
+        .as(ExitCode.Success)
+    }
 }
 
-object TomcatTestServer extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] =
-    TomcatBuilder[IO]
-      .bindHttp(8080)
-      .mountHttpApp(Main.app, "/")
-      .resource
-      .use(_ => IO.never)
-}
+// object JettyTestServer extends IOApp {
+//   override def run(args: List[String]): IO[ExitCode] =
+//     JettyBuilder[IO]
+//       .bindHttp(8080)
+//       .mountHttpApp(Main.app, "/")
+//       .resource
+//       .use(_ => IO.never)
+// }
 
-object FinagleTestServer extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = {
-    val server = IO(FHttp.server.serve(":8080", Finagle.mkService(Main.app)))
-    server.map(Await.ready(_)) >> IO.never
-  }
-}
+// object TomcatTestServer extends IOApp {
+//   override def run(args: List[String]): IO[ExitCode] =
+//     TomcatBuilder[IO]
+//       .bindHttp(8080)
+//       .mountHttpApp(Main.app, "/")
+//       .resource
+//       .use(_ => IO.never)
+// }
+
+// object FinagleTestServer extends IOApp {
+//   override def run(args: List[String]): IO[ExitCode] = {
+//     val server = IO(FHttp.server.serve(":8080", Finagle.mkService(Main.app)))
+//     server.map(Await.ready(_)) >> IO.never
+//   }
+// }
